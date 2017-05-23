50a6ebe7dfe93d7fcd2d4fcf30824cce40caf9a6
KimJuYoung93 <kjy@git.co.kr>
2017-05-23 17:53:47 +0900

    commit

--- OpenGDS_2017/src/main/java/com/git/gdsbuilder/convertor/impl/JsonFromUrl.java
+++ OpenGDS_2017/src/main/java/com/git/gdsbuilder/convertor/impl/JsonFromUrl.java
@@ -65,6 +65,7 @@ public class JsonFromUrl {
 	public SimpleFeatureCollection readJsonFromUrl(String collectionName, String layerName, int max, boolean flag,
 			String fileType) throws IOException, SchemaException {
 		System.out.println("test");
+		System.out.println("test");
 		try {
 			DataConvertor dtConvertor = new DataConvertorImpl();
 			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
