2b92dd326b78afa317aa8b22d4a7a0870dc1253a
GIT <GIT@GIT-PC>
2017-05-23 18:21:34 +0900

    test

--- OpenGDS_2017/src/main/java/com/git/gdsbuilder/convertor/impl/JsonFromUrl.java
+++ OpenGDS_2017/src/main/java/com/git/gdsbuilder/convertor/impl/JsonFromUrl.java
@@ -82,6 +82,7 @@ public class JsonFromUrl {
 		System.out.println("ttttttttttttttt");
 		System.out.println("왜안되지");
 		System.out.println("왜안되지");
+		System.out.println("왜안되지");
 
 		try {
 			DataConvertor dtConvertor = new DataConvertorImpl();
